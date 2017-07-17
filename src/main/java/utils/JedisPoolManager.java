1f46c72b296bb4092ad5f80c21e5434417fc93b3
HelloAsher <281664501@qq.com>
2017-07-17 19:49:08 +0800

    robin committed

---
+++ src/main/java/utils/JedisPoolManager.java
@@ -0,0 +1,39 @@
+package utils;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * Created by yzbfl on 2017-07-17.
+ */
+public class JedisPoolManager {
+    private volatile static JedisPoolManager jedisPoolManager;
+    private final JedisPool pool;
+
+    public JedisPoolManager() {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(8);
+        poolConfig.setMaxIdle(4);
+        this.pool = new JedisPool(poolConfig, "172.18.xxx.xxx", 6379);
+    }
+
+    public static JedisPoolManager getInstance(){
+        if(jedisPoolManager == null){
+            synchronized (JedisPoolManager.class){
+                if(jedisPoolManager == null){
+                    jedisPoolManager = new JedisPoolManager();
+                }
+            }
+        }
+        return jedisPoolManager;
+    }
+
+    public Jedis getResource(){
+        return this.pool.getResource();
+    }
+
+    public void close(){
+        pool.close();
+    }
+}
