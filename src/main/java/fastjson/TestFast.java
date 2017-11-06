d8f7512fd3b43fcdea573d9e43dd5956880756e0
HelloAsher <281664501@qq.com>
2017-11-06 18:49:03 +0800

    robin committed

--- src/main/java/fastjson/TestFast.java
+++ src/main/java/fastjson/TestFast.java
@@ -1,11 +1,15 @@
 package fastjson;
 
 import com.alibaba.fastjson.JSON;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class TestFast {
     public static void main(String[] args){
         MyObject myObject = new MyObject();
         String jsonString = String.format("{\"secID\": \"002860.XSHE\", \"ticker\": \"002860\", \"secShortName\": \"星帅尔\", \"cnSpell\": \"XSE\", \"exchangeCD\": \"XSHE\", \"assetClass\": \"E\", \"listStatusCD\": \"L\", \"listDate\": \"2017-04-12\", \"transCurrCD\": \"CNY\", \"partyID\": 66202}");
+        System.out.println(jsonString);
         myObject.setMyJson(jsonString);
         myObject.setOtherString("testJson");
         String jsonString1 = JSON.toJSONString(myObject);
@@ -14,5 +18,9 @@ public class TestFast {
 
         MyObject myObject1 = JSON.parseObject(jsonString1, MyObject.class);
         System.out.println(myObject1);
+
+        JSONObject jObject = new JSONObject(jsonString);
+        Object sdfas = jObject.get("sdfas");
+
     }
 }
